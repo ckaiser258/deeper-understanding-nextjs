9a1de4328a14ddcec2de92479b06eaaac5c75510
Colton Kaiser <kaiserc2@seattleu.edu>
2021-10-21 15:07:14 -0600

    Nested dynamic routes.

---
+++ pages/product/[productId].tsx
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+
+const ProductDetail = () => {
+  const router = useRouter();
+  //   query.productId because that's the name of the file
+  //   router.query.productId will return whatever number is after /product
+  // ex. if the url is /product/1, router.query.productId will return 1
+  //   this can also be any string (so /hello would return 'hello')
+  //   /sweater would NOT return this page as next is smart enough to check that there is a sweater file that exists here
+  const productId = router.query.productId;
+  return <h1>Details about product {productId}</h1>;
+};
+
+export default ProductDetail;
---
+++ pages/product/sweater.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sweater = () => {
+  return <h1>Landing page for Sweaters</h1>;
+};
+
+export default Sweater;
